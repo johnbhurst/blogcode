23efc2520eb7fb746135cd1747bb703e1b966bce
John Hurst <john.b.hurst@gmail.com>
2012-01-11 17:09:29 +1300

    Removed metadata example from here; added "normal" use of parameter lists and GStrings.

--- 2012-01-10-groovy-sql-string-vs-gstring/sql-strings.groovy
+++ 2012-01-10-groovy-sql-string-vs-gstring/sql-strings.groovy
@@ -1,10 +1,26 @@
-
 @Grab(group='com.oracle', module='ojdbc6', version='11.2.0.3.0')
 import oracle.jdbc.pool.OracleDataSource
 import groovy.sql.Sql
 
 String connectionString = args[0]
 Sql db = new Sql(new OracleDataSource(URL: "jdbc:oracle:thin:$connectionString"))
+
+// Normal use of query parameters:
+
+def office1 = db.firstRow("SELECT * FROM offices WHERE state = ? AND country = ?", ["CA", "USA"])
+assert office1.city == "San Francisco"
+
+def office2 = db.firstRow("SELECT * FROM offices WHERE state = ? AND country = ?", "CA", "USA")
+assert office2.city == "San Francisco"
+
+// Using GString:
+
+def state = "CA"
+def country = "USA"
+def office3 = db.firstRow("SELECT * FROM offices WHERE state = $state AND country = $country")
+assert office3.city == "San Francisco"
+
+// Where variable is *not* a SQL placeholder:
 
 def tables = db.rows("SELECT table_name FROM user_tables ORDER BY 1")*.table_name
 println tables.size()
@@ -14,13 +30,4 @@ tables.each {tableName ->
   def rowCount = db.firstRow("SELECT COUNT(*) c FROM $tableName".toString()).c
   println "$tableName: $rowCount"
 }
-
-def printHeader = {metaData ->
-  println metaData*.columnName.join(",")
-}
-db.eachRow("SELECT * FROM offices", printHeader) {row ->
-  println row.toRowResult().values().join(",")
-}
-
-
 
