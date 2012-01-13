bb2aa2be0e269d57cd9e3322439927fbd209c798
John Hurst <john.b.hurst@gmail.com>
2012-01-13 16:42:32 +1300

    Added copyright notice from Eclipse.

--- 2012-01-11-groovy-sql-embedded/birt-h2.sql
+++ 2012-01-11-groovy-sql-embedded/birt-h2.sql
@@ -1,3 +1,21 @@
+--*******************************************************************************
+--**  Copyright (c) 2005 Actuate Corporation.
+--**  All rights reserved. This file and the accompanying materials
+--**  are made available under the terms of the Eclipse Public License v1.0
+--**  which accompanies this distribution, and is available at
+--**  http://www.eclipse.org/legal/epl-v10.html
+--**
+--**  Contributors:
+--**  Actuate Corporation  - initial implementation
+--**
+--**  Classic Models Inc. sample database developed as part of the
+--**  Eclipse BIRT Project. For more information, see http:\\www.eclipse.org\birt
+--**
+--** *******************************************************************************
+--**
+--** This version adapted for H2 and Groovy-SQL examples by John Hurst (john.hurst@gmail.com)
+--**
+--** *******************************************************************************
 
 CREATE TABLE Offices(
 	officeCode VARCHAR(10),
